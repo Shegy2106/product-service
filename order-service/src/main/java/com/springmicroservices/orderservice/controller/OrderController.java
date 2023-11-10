9acf3abd3a1990499c5f24045e5829acfb7c1d2a
Shegy2106 <darko.segvic@gmail.com>
2023-11-10 14:40:12 +0100

    order service

---
+++ order-service/src/main/java/com/springmicroservices/orderservice/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.springmicroservices.orderservice.controller;
+
+import com.springmicroservices.orderservice.dto.OrderRequest;
+import com.springmicroservices.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
