414c42f1b50d293cb123676081850852c00f26c1
Shegy2106 <darko.segvic@gmail.com>
2023-11-29 13:16:22 +0100

    fixing notification service being undiscoverable/unconnectable

--- notification-service/src/main/java/com/springmicroservices/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/springmicroservices/notificationservice/NotificationServiceApplication.java
@@ -5,9 +5,11 @@ import com.springmicroservices.notificationservice.event.OrderPlacedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Slf4j
 public class NotificationServiceApplication {
 
